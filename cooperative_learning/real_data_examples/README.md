a7a30bfbda44fcbea014f7c013b6c1d21b7dd9d4
Tony Liang <chunqingliang@gmail.com>
2023-01-15 18:50:36 -0800

    update README for real_data_examples

--- cooperative_learning/real_data_examples/README.md
+++ cooperative_learning/real_data_examples/README.md
@@ -1,3 +1,8 @@
 # Real Data Examples
 
+
+## Labor Onset
 There is an onset-labor proteomics data, please reference [here](https://zenodo.org/record/4509768#.Y8S6z3bMIQ8)
+
+## DCIS Breast Cancer
+There is two data csv files of breast cancer
---
+++ cooperative_learning/real_data_examples/dcis_breast_cancer/dsci_classification.R
@@ -0,0 +1,11 @@
+# Change this FUN_PATH
+FUN_PATH <- "real_data_examples/dcis_breast_cancer/cooperative_learning_logistic_regression.R"
+source(FUN_PATH)
+
+# Import libraries
+library(caret)
+library(glmnet)
+library(multiview)
+
+
+# read csvs
